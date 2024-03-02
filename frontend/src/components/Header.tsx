c54c1396189095b29e3b1888c3871a43a036b9b2
Govind <govind151201@gmail.com>
2024-03-02 19:38:22 +0530

    Mobile nav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,20 @@
 
 import { Link } from 'react-router-dom'
+import MobileNav from './MobileNav'
 
-function Header() {
-  return (
-    <div className='border-b-2 border-b-orange-500 py-6'>
-      <div className="container mx-auto flex justify-between items-center">
-        <Link 
-          to="/"
-        className='text-3xl font-bold tracking-tight text-orange-500'>MernEats.com</Link>
-      </div>
-    </div>
-  )
+const Header = () => {
+    return (
+        <div className='border-b-2 border-b-orange-500 py-6'>
+            <div className="container mx-auto flex justify-between items-center">
+                <Link
+                    to="/"
+                    className='text-3xl font-bold tracking-tight text-orange-500'>MernEats.com</Link>
+                <div className="md:hidden">
+                    <MobileNav />
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Header
+export default Header;
